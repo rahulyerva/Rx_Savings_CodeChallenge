ea3d6207a779ba1335d067da378a5a265609d813
rahulyerva <ryfh3@mail.umkc.edu>
2021-02-23 21:06:48 -0600

    Updated tfvars

--- Terraform_Code/variables.tf
+++ Terraform_Code/variables.tf
@@ -1,17 +1,5 @@
-variable "fixed_response_content_type" {
-  default     = "text/plain"
+variable "alb_name" {
   type        = string
-  description = "The content type. Valid values are text/plain, text/css, text/html, application/javascript and application/json."
+  description = "Name of application load balancer"
 }
 
-variable "fixed_response_message_body" {
-  default     = "404 Not Found"
-  type        = string
-  description = "The message body."
-}
-
-variable "fixed_response_status_code" {
-  default     = "404"
-  type        = string
-  description = "The HTTP response code. Valid values are 2XX, 4XX, or 5XX."
-}
